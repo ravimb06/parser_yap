018084d279c6981e114de34c3ab8ab13d2100842
Ali Bogatyrev <marksheyder06@gmail.com>
2022-11-23 00:10:52 +0300

    added configs

---
+++ configs.py
@@ -0,0 +1,23 @@
+import argparse
+
+
+def configure_argument_parser(available_modes):
+    parser = argparse.ArgumentParser(description='Парсер документации Python')
+    parser.add_argument(
+        'mode',
+        choices=available_modes,
+        help='Режимы работы парсера'
+    )
+    parser.add_argument(
+        '-c',
+        '--clear-cache',
+        action='store_true',
+        help='Очистка кеша'
+    )
+    parser.add_argument(
+        '-o',
+        '--output',
+        choices=('pretty', 'file'),
+        help='Дополнительные способы вывода данных'
+    )
+    return parser
